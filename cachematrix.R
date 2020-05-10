6b22129cf664727bc13d55c429f8e89f4e6fd2cc
Zelalem Tesfaye Solon <64289968+Zelalemsolon@users.noreply.github.com>
2020-05-10 01:57:33 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## The overall objective this project is to catch a matrix and find its inverse.
+## This function will catch the matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+       Minv<-NULL
+  set<- function(y){
+    x<<- y
+    Minv<<- NULL 
 }
-
-
-## Write a short comment describing this function
-
+get<- function() x
+setinverse<- function(inverse) Minv<<- inverse
+getinverse<- function() Minv
+list(set=set, get=get,
+     setinverse=setinverse, getinverse=getinverse)
+}
+## This function will solve the inverse of the given matrix.
+##It will check if the inverse exists, if not it will compute the inverse.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  Minv<- x$getinverse()
+  if(!is.null(Minv)){
+    message("getting catched data.")
+    return(Minv) 
+}
+data<- x$get()
+  Minv<- solve(data)
+  x$setinverse(Minv)
+  Minv 
 }
